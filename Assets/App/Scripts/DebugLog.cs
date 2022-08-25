5bfe903d97d0ef212fa00c8d3ca57bc54d652f2f
trustred <trust870@hotmail.com>
2022-08-25 22:33:18 +0200

    debug log for builds

---
+++ Assets/App/Scripts/DebugLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Gabsee
+{
+    public class DebugLog : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI debugText;
+        private StringBuilder log = new StringBuilder();
+
+        private void Awake() => Application.logMessageReceived += LogMessageReceived;
+        
+        private void LogMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            log.AppendLine($"{condition}");
+            debugText.text = log.ToString();
+        }
+    }
+}
